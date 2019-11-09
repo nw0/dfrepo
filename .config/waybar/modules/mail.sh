73d67633ff1acf4355e25d8538f0c2d10de60796
Nicholas Sim <nsim@posteo.net>
2019-11-09 23:05:31 +0800

    waybar: add icons, remove colour

--- .config/waybar/modules/mail.sh
+++ .config/waybar/modules/mail.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 unread=$(mailcheck -bcs | grep unread | awk '{ print $5 }')
-echo -e '{"text": "m '$(mailcheck -bcs | grep unread | awk '{ print $5 }')'"}'
+echo -e '{"text": "'$(mailcheck -bcs | grep unread | awk '{ print $5 }')'"}'
