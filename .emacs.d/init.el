c932c386d1fa2e7fc7acade372cc4d9a54ad8a83
Nicholas Sim <nsim@posteo.net>
2019-11-15 20:32:45 +0800

    emacs: move package code out

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,17 +1,6 @@
 ;; -*- mode: emacs-lisp -*-
 
-;; Always load packages first!
-;; Package: elpa, melpa
-(setq gnutls-min-prime-bits 4096)
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-
-;; Disable the splash screen (to enable it again, replace the t with 0)
-;; This messes with line numbering and who knows what else
-(setq inhibit-splash-screen t
-      sentence-end-double-space nil)
-
+(load-file "~/.emacs.d/packages.el")
 ;; Custom things
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
