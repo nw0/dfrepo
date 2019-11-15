b94bf08caf642fb489afc153e5df198b8eafd7df
Nicholas Sim <nsim@posteo.net>
2019-11-15 18:57:14 +0800

    emacs: don't gc so much

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,3 +18,11 @@
 
 (load-file "~/.emacs.d/interface.el")
 (load-file "~/.emacs.d/org.el")
+
+;; less frequent garbage collection
+(setq gc-cons-threshold (* 100 1024 1024)) ;; 100 mb
+;; Allow font-lock-mode to do background parsing
+(setq jit-lock-stealth-time 1
+      ;; jit-lock-stealth-load 200
+      jit-lock-chunk-size 1000
+      jit-lock-defer-time 0.05)
