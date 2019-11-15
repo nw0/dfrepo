41b7ca82702d04ef80b53d33f136fff0b4a2c1e9
Nicholas Sim <nsim@posteo.net>
2019-11-15 20:33:38 +0800

    emacs: interface: more customisation

--- .emacs.d/interface.el
+++ .emacs.d/interface.el
@@ -10,7 +10,21 @@
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
+(setq coding-system-for-read 'utf-8
+      coding-system-for-write 'utf-8)
 (setq default-buffer-file-coding-system 'utf-8)
+
+;; adapted from https://sam217pa.github.io/2016/09/02/how-to-build-your-own-spacemacs/
+(setq delete-old-versions -1            ; delete excess backup versions silently
+      version-control t                 ; use version control
+      vc-make-backup-files t            ; make backups file even when in version controlled dir
+      backup-directory-alist `(("." . "~/.emacs.d/backups"))    ; which directory to put backups file
+      vc-follow-symlinks t              ; don't ask for confirmation when opening symlinked file
+      auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t))   ; transform backups file name
+      inhibit-startup-screen t          ; inhibit useless and old-school startup screen
+      ring-bell-function 'ignore        ; silent bell when you make a mistake
+      sentence-end-double-space nil     ; sentence SHOULD end with only a point.
+      default-fill-column 79)           ; toggle wrapping text at the 80th character
 
 ;; disable a buffer's mark on defocus
 (transient-mark-mode 1)
@@ -23,28 +37,48 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-(line-number-mode 1)
-(column-number-mode 1)
-;; nice lambdas
-(global-prettify-symbols-mode t)
-;; syntax highlighting
-(global-font-lock-mode t)
+(line-number-mode 1)                ; default, but be sure
+(column-number-mode 1)              ; in the modeline
+(blink-cursor-mode -1)              ; this is annoying (graphical only)
+(global-prettify-symbols-mode t)    ; nice lambda symbols
+(global-font-lock-mode t)           ; syntax highlighting
+(global-hl-line-mode t)
 
-;; highlight matching parentheses
-(show-paren-mode t)
-(setq show-paren-delay 0.0)
+(show-paren-mode t)                 ; highlight matching parentheses
+(setq show-paren-delay 0.0
+      blink-matching-paren nil)
 
 (setq-default tab-width 4
               indent-tabs-mode nil)
-(setq require-final-newline t)
+(setq require-final-newline t)      ; less insanity
+(add-hook 'before-save-hook 'whitespace-cleanup)    ; no more trailing ws
+
 
 ;; ahh
 (global-set-key (kbd "<home>") 'beginning-of-line)
 (global-set-key (kbd "<end>") 'end-of-line)
 
+;; hide some modes
+(use-package diminish
+  :ensure t
+  :config
+  (diminish 'eldoc-mode))
+
+;; like this one
+(use-package which-key
+  :ensure t
+  :diminish which-key-mode
+  :init (which-key-mode)
+  :config
+  (which-key-setup-side-window-bottom)
+  (setq which-key-idle-delay 0.5))
+
+
 ;; Theme: this won't load properly if it's before custom-safe-themes
-;; alternatively, unconditionally load with t
-(load-theme 'gruvbox)
+(use-package gruvbox-theme
+  :ensure t
+  :config
+  (load-theme 'gruvbox))
 
 ;; for graphical emacs
 (set-face-attribute 'default nil :height 140)
