12e1c7cac8418e67d792bff7e7cafc4e7253abf9
Nicholas Sim <nsim@posteo.net>
2019-11-17 16:42:19 +0800

    emacs: org: config

--- .emacs.d/org.el
+++ .emacs.d/org.el
@@ -1,3 +1,10 @@
 ;;;Org mode configuration
-;; Enable Org mode
-(require 'org)
+(use-package org
+  :bind (("C-c a" . org-agenda))
+  :config
+  (setq org-src-preserve-indentation t
+        org-todo-keywords '((sequence "TODO(t)" "BEGUN(s)" "WAIT(w)" "|" "DONE(d)" "DROP(c)"))
+        org-startup-folded nil
+        org-startup-indented 'f
+        org-agenda-files '("~/org"))
+  (eval-after-load 'org-indent '(diminish 'org-indent-mode)))
