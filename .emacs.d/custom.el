1d1ad192b58a1eb1a99217734ffd9b0f154d1099
Nicholas Sim <nsim@posteo.net>
2019-11-26 22:27:27 +0800

    emacs: update custom package list

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -8,12 +8,11 @@
     ("1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" default)))
  '(package-selected-packages
    (quote
-    (ledger-mode use-package evil-collection gruvbox-theme))))
+    (markdown-mode mixed-pitch smart-hungry-delete pangu-spacing aggressive-indent ledger-mode use-package evil-collection gruvbox-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
  '(fixed-pitch ((t (:family "Fira Mono"))))
  '(variable-pitch ((t (:family "Nimbus Sans")))))
