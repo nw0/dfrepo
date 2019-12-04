3e7fb4a0d13c2c2cf0af84c3966956f3bf883acd
Nicholas Sim <nsim@posteo.net>
2019-12-04 20:47:16 +0800

    emacs: unicode, ws, highlight

--- .emacs.d/edit.el
+++ .emacs.d/edit.el
@@ -1,12 +1,3 @@
-;; use utf-8
-(prefer-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(setq coding-system-for-read 'utf-8
-      coding-system-for-write 'utf-8)
-(setq default-buffer-file-coding-system 'utf-8)
-
 (global-prettify-symbols-mode t)    ; nice lambda symbols
 (electric-pair-mode 1)              ; auto-insert closing parens, and more
 
@@ -14,8 +5,6 @@
               tab-stop-list '(4 8 12)
               indent-tabs-mode nil
               show-trailing-whitespace t)
-(setq require-final-newline t)      ; less insanity
-(add-hook 'before-save-hook 'whitespace-cleanup)    ; no more trailing ws
 
 ;; amazing completion
 (use-package company
