0794ceadcf3da279d6f62edb215006a1552c5c48
Nicholas Sim <nsim@posteo.net>
2019-12-08 21:12:43 +0800

    emacs: small improvements

--- .emacs.d/org.el
+++ .emacs.d/org.el
@@ -12,8 +12,7 @@
         org-todo-keywords '((sequence "TODO(t)" "BEGUN(s)" "WAIT(w)" "|" "DONE(d)" "DROP(c)"))
         org-startup-folded nil
         org-startup-indented 'f
-        org-agenda-files '("~/org")
-        org-hide-emphasis-markers t)
+        org-agenda-files '("~/org"))
   (eval-after-load 'org-indent '(diminish 'org-indent-mode))
   )
 
