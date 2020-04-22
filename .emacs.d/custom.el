092dcba50b44515326d262b2035deb250dee1d5d
Nicholas Sim <nsim@posteo.net>
2020-04-22 17:32:57 +0800

    emacs: many modes

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -9,7 +9,7 @@
     ("4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" "aded61687237d1dff6325edb492bde536f40b048eab7246c61d5c6643c696b7f" "b89ae2d35d2e18e4286c8be8aaecb41022c1a306070f64a66fd114310ade88aa" "15ba8081651869ec689c9004288bed79003de5b4ee9c51a9d4a208d9e3439706" "1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" default)))
  '(package-selected-packages
    (quote
-    (flycheck-rust racer cargo irony ivy-rich ivy swiper counsel smart-mode-line ripgrep rg rust-mode projectile doom-themes rainbow-mode edit-indirect highlight-symbol smooth-scroll deadgrep ws-butler fic-mode auto-highlight-symbol flycheck yaml-mode company-anaconda anaconda-mode markdown-mode mixed-pitch smart-hungry-delete pangu-spacing aggressive-indent ledger-mode use-package evil-collection gruvbox-theme))))
+    (toml-mode flycheck-rust racer cargo irony ivy-rich ivy swiper counsel smart-mode-line ripgrep rg rust-mode projectile doom-themes rainbow-mode edit-indirect highlight-symbol smooth-scroll deadgrep ws-butler fic-mode auto-highlight-symbol flycheck yaml-mode company-anaconda anaconda-mode markdown-mode mixed-pitch smart-hungry-delete pangu-spacing aggressive-indent ledger-mode use-package evil-collection gruvbox-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
