a5b299ddf4f0a2acfd0843a3c49df8e8bc40f2a0
Nicholas Sim <nsim@posteo.net>
2020-05-29 21:46:51 +0800

    emacs: some cleanup

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,23 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-idle-delay 0.2 t)
  '(custom-safe-themes
    (quote
-    ("4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" "aded61687237d1dff6325edb492bde536f40b048eab7246c61d5c6643c696b7f" "b89ae2d35d2e18e4286c8be8aaecb41022c1a306070f64a66fd114310ade88aa" "15ba8081651869ec689c9004288bed79003de5b4ee9c51a9d4a208d9e3439706" "1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" default)))
- '(package-selected-packages
-   (quote
-    (toml-mode flycheck-rust racer cargo irony ivy-rich ivy swiper counsel smart-mode-line ripgrep rg rust-mode projectile doom-themes rainbow-mode edit-indirect highlight-symbol smooth-scroll deadgrep ws-butler fic-mode auto-highlight-symbol flycheck yaml-mode company-anaconda anaconda-mode markdown-mode mixed-pitch smart-hungry-delete pangu-spacing aggressive-indent ledger-mode use-package evil-collection gruvbox-theme))))
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" "b89ae2d35d2e18e4286c8be8aaecb41022c1a306070f64a66fd114310ade88aa" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t (:family "Fira Mono"))))
  '(org-document-info ((t (:foreground "#98971a"))))
  '(org-document-title ((t (:foreground "#98971a"))))
  '(org-property-value ((t (:foreground "yellow green"))) t)
  '(org-verbatim ((t (:inherit shadow :foreground "gray"))))
  '(sml/filename ((t (:foreground "salmon" :weight bold))))
  '(sml/modified ((t (:inherit sml/not-modified :foreground "salmon" :weight bold))))
- '(variable-pitch ((t (:family "Nimbus Sans")))))
+ )
