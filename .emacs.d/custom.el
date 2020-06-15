d30a347ac04f43e6ea1c0ddecd6ccfd98cf17399
Nicholas Sim <nsim@posteo.net>
2020-06-15 21:03:43 +0800

    emacs: elfeed: add

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,14 +3,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-idle-delay 0.2)
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" "b89ae2d35d2e18e4286c8be8aaecb41022c1a306070f64a66fd114310ade88aa" default))))
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" "b89ae2d35d2e18e4286c8be8aaecb41022c1a306070f64a66fd114310ade88aa" default)))
+ '(elfeed-goodies/entry-pane-size 0.6)
+ '(elfeed-goodies/switch-to-entry nil)
+ '(elfeed-goodies/tag-column-width 16)
+ '(elfeed-goodies/wide-threshold 0.36)
+ '(elfeed-sort-order (quote ascending)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(elfeed-search-title-face ((t (:foreground "#928374" :family "Liberation Sans"))))
  '(org-document-info ((t (:foreground "#98971a"))))
  '(org-document-title ((t (:foreground "#98971a"))))
  '(org-property-value ((t (:foreground "yellow green"))) t)
