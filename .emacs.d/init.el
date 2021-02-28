e5731bf9a1cae3806a6e075cdb0a90ede9edd83e
Nicholas Sim <nsim@posteo.net>
2021-02-28 15:19:02 +0800

    emacs: fix org mode config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -145,12 +145,6 @@
 (use-package org
   :bind (:map org-mode-map
               ("C-c a" . org-agenda))
-  :hook
-  ;; fix the indentation for variable-pitch
-  (after-init . (lambda ()
-                  (require 'org-indent)
-                  (set-face-attribute 'org-indent nil
-                                      :inherit '(org-hide fixed-pitch))))
   :config
   (setq org-src-preserve-indentation t
         org-todo-keywords '((sequence "TODO(t)" "BEGUN(s)" "WAIT(w)" "|" "DONE(d)" "DROP(c)"))
@@ -164,6 +158,7 @@
         org-agenda-files '("~/org"))
   (eval-after-load 'org-indent '(diminish 'org-indent-mode))
   (setq org-latex-pdf-process (list "latexmk -f -pdf %f"))
+  (require 'org-indent)
   )
 
 
@@ -203,7 +198,7 @@
            :head "#+TITLE: ${title}\n\n- affiliation ::")
           ("p" "resource" plain (function org-roam--capture-get-point)
            "%?"
-           :file-name "%<%Y%m%d-${slug}"
+           :file-name "%<%Y%m%d>-${slug}"
            :head ,(concat
                    "#+TITLE: ${title}\n\n"
                    "- tags :: \n"
