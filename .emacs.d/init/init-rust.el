cda197b0efdb1033ec80e165fa3729a34b32c3b1
Nicholas Sim <nsim@posteo.net>
2021-02-28 15:19:49 +0800

    emacs: use rust-mode
    rustic seems to have broken completion

--- .emacs.d/init/init-rust.el
+++ .emacs.d/init/init-rust.el
@@ -8,14 +8,14 @@
 ;;; Code:
 
 ;; Found this to be faster than rust-mode
-(use-package rustic
+(use-package rust-mode
   :straight t
   :defer t
 ;;  :mode "\\.rs\\'"
-  :bind (:map rustic-mode-map ("M-<return>" . comment-indent-new-line))
+  :bind (:map rust-mode-map ("M-<return>" . comment-indent-new-line))
   :config
-  (define-key rustic-mode-map (kbd "TAB") #'company-indent-or-complete-common)
-  (setq rustic-format-on-save nil))
+  (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+  (setq rust-format-on-save nil))
 
 ;; Doesn't work, json-read-error
 (use-package flycheck-rust
